0bca5182263c94f38631028eaa03162ea960cd99
Evgeny <tamplier77@gmail.com>
2017-11-18 21:13:27 +0200

    fix urllib import

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import urllib2
-content = urllib2.urlopen('http://127.0.0.1:5000/').read()
+from urllib.request import urlopen
+content = urlopen('http://127.0.0.1:5000/').read()
 if content == "Looks OK.":
     exit(0)
 else:
